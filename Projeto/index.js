bc53c063115eb9f55260c75742a9bd09177a4a55
ALAN LOPES MELO <allanlopesmelo@gmail.com>
2022-09-06 22:46:42 -0300

    Adicionando pastas e servidor node

---
+++ Projeto/index.js
@@ -0,0 +1,25 @@
+//Aplication IMPORTS
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import aes256 from "aes256";
+
+//Encriptografia
+var key = 'bf3c199c2470cb477d907b1e0917c17b';
+var cipher = aes256.createCipher(key);
+//Ex:
+//	cipher.encrypt("senha")
+//	cipher.decrypt("senha");
+
+const server = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+server.use(express.json()) //habilita o uso de JSONS
+server.use(express.urlencoded({ extended: true })); //habilita o uso do post dentro das rotas
+server.use(express.static(path.join(__dirname + "/public"))); //habilita o uso de arquivos estaticos
+server.set("views", path.join(__dirname + "/public/views")); //define a pasta de views
+
+server.listen(3000, () => {
+	console.log(`Server is running on port 3000`);
+});
